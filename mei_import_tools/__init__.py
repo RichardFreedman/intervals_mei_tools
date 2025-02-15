78296d29b38215a07e57e8b9f94803fe0cb03b8b
Richard Freedman <rfreedma@haverford.edu>
2025-02-15 11:48:05 +0100

    update import and toml

--- mei_import_tools/__init__.py
+++ mei_import_tools/__init__.py
@@ -1,5 +1,21 @@
 # __init__.py
-from mei_import_tools import GitHubFileManager, GitHubConfig
+def get_github_manager():
+    from mei_import_tools import GitHubFileManager
+    return GitHubFileManager
+
+def get_config():
+    from mei_import_tools import GitHubConfig
+    return GitHubConfig
 
 __package__ = __name__
-__all__ = ['GitHubFileManager', 'GitHubConfig']
+__all__ = ['get_github_manager', 'get_config']
+
+
+
+# old version
+# __init__.py
+# from mei_import_tools import GitHubFileManager, GitHubConfig
+
+# __package__ = __name__
+# __all__ = ['GitHubFileManager', 'GitHubConfig']
+
