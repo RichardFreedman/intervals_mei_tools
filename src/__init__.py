4429a57f44c2ae240d7ce454ca025ace9a247a3e
Richard Freedman <rfreedma@haverford.edu>
2025-02-15 10:52:00 +0100

    add new innit and toml

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,5 @@
 # __init__.py
-from .mei_import_tools import GitHubFileManager, GitHubConfig
+from mei_import_tools import GitHubFileManager, GitHubConfig
 
 __package__ = __name__
 __all__ = ['GitHubFileManager', 'GitHubConfig']
